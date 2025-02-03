a597de4e7c4afda61f8b8dd687c65f1dbb2e81d8
Practice <prince15aj13@gmail.com>
2025-02-03 23:13:12 +0600

    added pics

--- script/video.js
+++ script/video.js
@@ -13,6 +13,89 @@ fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
 .catch((error)=>console.log(error))
 }
 
+
+const loadVideos= () =>{
+
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then((res) => res.json())
+    .then((data)=>displayVideos(data.
+        videos))
+    .catch((error)=>console.log(error))
+    }
+// const cardDemo= {
+//     "category_id": "1003",
+//     "video_id": "aaaf",
+//     "thumbnail": "https://i.ibb.co/5LRQkKF/stick-and-stones.jpg",
+//     "title": "Sticks & Stones",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/rdTZrCM/dev.jpg",
+//             "profile_name": "Dave Chappelle",
+//             "verified": true
+//         }
+//     ],
+//     "others": {
+//         "views": "113K",
+//         "posted_date": ""
+//     },
+//     "description": "Dave Chappelle's 'Sticks & Stones' has garnered 113K views and remains a controversial yet highly engaging piece of stand-up comedy. Known for his fearless approach, Dave dives into a wide range of topics, delivering his unique perspective with wit and sharp humor. As a verified artist, Dave's comedy is raw, honest, and unapologetically funny."
+// }
+    
+
+
+  const displayVideos=(videos) =>
+    {
+
+        const videoContainer =document.getElementById("videos")
+videos.forEach(video =>{
+
+console.log(video)
+const card = document.createElement('div')
+card.classList="card card-compact "
+card.innerHTML = ` <figure class="h-[200px]">
+    <img
+      src=${video.thumbnail}
+    class=h-full w-full object-cover
+      alt="Shoes" />
+  </figure>
+  <div class=" px-0 py-2 flex gap-2">
+<div>
+<img class= "w-10 h-10 rounded-full" src=${video.authors[0].profile_picture}/>
+</div>    
+    
+
+
+<div>
+<h2 class="font-bold">
+${video.title}
+
+</h2>
+<div class="flex items-center gap-2">
+
+<p class="text-gray-400">${video.authors[0].profile_name}</p>
+<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png">
+</div>
+</div>
+<p>
+
+</p>
+<p>
+
+</p>
+
+
+</h2>
+</div>    
+
+
+</div>
+  `
+
+
+  videoContainer.append(card)
+})
+
+    }  
 // Create Display catagories
 
 const displayCatagories = (catagories)=>{
@@ -34,3 +117,4 @@ catagoryContainer.append(button)
 
 
 loadCatagories()
+loadVideos()
